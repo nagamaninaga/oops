6c3e033105aadf374a1198b2843ea95733217f36
nagamaninaga <111356935+nagamaninaga@users.noreply.github.com>
2022-08-25 22:57:40 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,13 @@
+
+calculator-grid > button 
+{
+    cursor: pointer;
+    font-size: 2rem;
+    border: 1px, solid #FFFFFF;
+    outline: none;
+    background-color: rbga(255, 255, 255, 0.75);
+  }
+
+    .calculator-grid > button:hover {
+      background-color: #a9a9a9;
+    }
---
+++ persn.js
@@ -0,0 +1,27 @@
+public class Person
+ {
+ name;
+  constructor(name)
+   {
+      this.name = name;
+    }
+  
+    introduceSelf() 
+    {
+      console.log(`Hi! I'm {kumar}`);
+    }
+}
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 5;
+        this.name = kumar;
+    }
+
+    public void printPerson() {
+        System.out.println(this.name + ", age " + this.age + " years");
+    }
+}
